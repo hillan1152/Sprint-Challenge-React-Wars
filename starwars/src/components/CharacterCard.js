cad302e16d01d15b5db193b983c9ac347b4c8c18
Anthony Hill <hillan1152@gmail.com>
2019-09-13 13:37:03 -0400

    finished

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Card, Button, CardTitle, CardText } from 'reactstrap';
+import styled from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CardDiv = styled.div`
+    border-radius: 15px;
+    width: 30%;
+    margin-left: 35%;
+
+    
+`;
+
+
+
 
 const CharacterCard = props => {
     return (
-        <div className="character-list" key={props.id}>
-        <Card body>
-            <CardTitle>{props.name}</CardTitle>
-            <CardText>Height: {props.height} Mass: {props.mass}</CardText>
-            <Button src={props.films}>Films</Button>
-        </Card>
-            {/* <h2>{props.name}</h2>
-            <p>Height: {props.height} Mass: {props.mass}</p>
-            <p>Films: {props.films}</p> */}
-        </div>
+        <CardDiv className="character-list" key={props.id}>
+            <Card body>
+                <CardTitle>{props.name}</CardTitle>
+                <CardText>Height: {props.height} Mass: {props.mass}</CardText>
+                <Button src={props.films}>Films</Button>
+            </Card>
+        </CardDiv>
     )
 }
 
